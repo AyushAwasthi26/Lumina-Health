66a603566e3dc25fc3f217c0a963d0e1a5563954
Ayush Awasthi <ayushawasthi382@gmail.com>
2026-07-12 12:40:01 +0530

    Added smooth scroll using lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import HealthcareApp from "./pages/HealthcareApp";
 
+// Smooth scroll adding using Lenis
+
+import { ReactLenis } from 'lenis/react';
+
+// Required: Prevents browser default scroll bouncing and layout jumps
+import 'lenis/dist/lenis.css';
+
 function App() {
-  return <HealthcareApp />;
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, syncTouch: true }}>
+      <HealthcareApp />
+    </ReactLenis>
+  );
 }
 
 export default App;
